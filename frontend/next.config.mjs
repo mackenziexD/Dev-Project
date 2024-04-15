181ec7849290cbf10fd6d9599798e7e73f618d48
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-04-15 18:12:44 +0100

    fixing actions

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     typescript: {
-      ignoreBuildErrors: true,
+        ignoreBuildErrors: true,
     },
     eslint: {
         ignoreDuringBuilds: true,
