631b3d8a721e521330b83bf26ee192acaba3e6df
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-04-15 17:36:17 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'c0027816-devproject-django.azurewebsites.net']
 
 # JWT/API
 REST_FRAMEWORK = {
