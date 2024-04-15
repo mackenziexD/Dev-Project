878aec59277b9acf3bcbbefa6b650eb3b13047de
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-04-15 18:08:22 +0100

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
     eslint: {
-        ignoreBuildErrors: true,
         ignoreDuringBuilds: true,
     },
 };
