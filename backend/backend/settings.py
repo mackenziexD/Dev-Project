5e245cb3256faa7e4bdb94f54b4baab74a97829b
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-04-23 00:09:28 +0100

    fixing CORS_ALLOWED_ORIGINS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,6 +108,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "http://localhost:3000",
     "http://c0027816-devproject-react.azurewebsites.net",
     "https://c0027816-devproject-react.azurewebsites.net",
 ]
